ab2f69119026bbdf2fff723da9cc837a8a9d566e
Harpreetsharmavats <harpreetsharma902@gmail.com>
2026-05-15 17:33:23 +0530

    UI Implementation

--- app/src/main/java/com/example/cleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/MainActivity.kt
@@ -1,26 +1,80 @@
 package com.example.cleanarchitecture
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.cleanarchitecture.presentation.ui.ViewModel
 import com.example.cleanarchitecture.presentation.ui.theme.CleanArchitectureTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: ViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CleanArchitectureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    innerPadding
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+                var query by rememberSaveable() { mutableStateOf("") }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TextField(modifier = Modifier.fillMaxWidth(),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            value = query,
+                            onValueChange = {
+                                query = it
+                                viewModel.updateQuery(query.toInt())
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    if(uiState.isLoading){
+                        Box(modifier = Modifier.padding(innerPadding),
+                            contentAlignment = Alignment.Center){
+                            CircularProgressIndicator()
+                        }
+                    }
+                    if (uiState.error.isNotEmpty()){
+                        Box(modifier = Modifier.padding(innerPadding),
+                            contentAlignment = Alignment.BottomCenter){
+                            Text(uiState.error)
+                        }
+                    }
+                    uiState.result.let { result ->
+                        LazyColumn(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                            items(result){
+
+                            }
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/example/cleanarchitecture/presentation/ui/ViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecture/presentation/ui/ViewModel.kt
@@ -6,10 +6,15 @@ import com.example.cleanarchitecture.domain.model.DomainUserResponse
 import com.example.cleanarchitecture.domain.useCases.GetUserUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class ViewModel : ViewModel() {
@@ -19,21 +24,35 @@ class ViewModel : ViewModel() {
     private val _state = MutableStateFlow(UiState())
     val uiState = _state.asStateFlow()
 
-    private val _query = MutableStateFlow("")
-    val query = _query.asStateFlow()
+    private val query = MutableStateFlow(0)
+
+    fun updateQuery(q : Int){
+        query.update { q }
+    }
 
     init {
         viewModelScope.launch {
-            _query.map { it.isNotEmpty() }
+            query.map { it }
                 .distinctUntilChanged()
                 .debounce(1000)
                 .collectLatest { query->
-
+                 getUsers(query)
                 }
 
         }
     }
     fun getUsers(q : Int){
+        useCase(q)
+            .onStart { _state.update { UiState(isLoading = true) } }
+            .onEach { result ->
+                if (result.isSuccess){
+                    _state.update { UiState(result = result.getOrThrow()) }
+                }else{
+                    _state.update { UiState(error = result.exceptionOrNull()?.message.toString()) }
+                }
+            }.catch{
+                _state.update { UiState(error = it.toString()) }
+            }.launchIn(viewModelScope)
 
     }
 
@@ -41,6 +60,6 @@ class ViewModel : ViewModel() {
 
 data class UiState(
     val isLoading: Boolean = false,
-    val result: List<DomainUserResponse>? = emptyList(),
+    val result: List<DomainUserResponse> = emptyList(),
     val error: String = ""
 )
